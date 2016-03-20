a4472556b8a4c9b4e094e06f104edf9de26dbdb6
io <disanzo@dis.uniroma1.it>
2016-03-20 14:23:20 +0100

    a

--- tinySTM/src/stm.c
+++ tinySTM/src/stm.c
@@ -655,7 +655,7 @@ inline void stm_tune_scheduler() {
 
 	last_throughput = current_throughput;
 	last_tuning_time = STM_TIMER_READ();
-	printf("\nActive_threads %i",active_threads);
+	active_threads=5;
 	fflush(stdout);
 }
 
